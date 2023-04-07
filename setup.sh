f6b6956217b801eb6f494f0057c9b0d03632121a
t1gr3 <wamg-85@hotmail.com>
2023-04-06 23:45:49 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,15 +6,15 @@ unzip $PWD/src/fonts.zip
 sudo mv $PWD/*.ttf /usr/share/fonts/truetype/hack
 mkdir ~/.config/i3
 mkdir ~/.config/terminator
-mkdir ~/.config/bumblebee-status
+#mkdir ~/.config/bumblebee-status
 mkdir ~/.config/compton
 mkdir ~/.config/rofi
 mkdir ~/.config/wallpaper
 cp $PWD/src/i3/* ~/.config/i3
 cp $PWD/src/terminator/* ~/.config/terminator
-git clone https://github.com/tobi-wan-kenobi/bumblebee-status.git
-sleep 300
-cp -p $PWD/bumblebee-status/* $HOME/.config/bumblebee-status
+git clone $HOME/.config/https://github.com/tobi-wan-kenobi/bumblebee-status.git
+sleep 50
+#cp -p $PWD/bumblebee-status/* $HOME/.config/bumblebee-status
 cp $PWD/src/compton/* ~/.config/compton
 cp $PWD/src/rofi/* ~/.config/rofi
 cp $PWD/src/wallpaper/* ~/.config/wallpaper
