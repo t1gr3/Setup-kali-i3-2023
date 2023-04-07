1ff2c7fe13f7d197b48097453a560830aaace066
t1gr3 <wamg-85@hotmail.com>
2023-04-06 20:58:27 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,17 +2,19 @@
 
 sudo apt update
 sudo apt install i3 dolphin terminator rofi feh flameshot compton python3-psutil -y
+git clone git://github.com/tobi-wan-kenobi/bumblebee-status
+cp ~/.config/bumblebee-status/*
 unzip $PWD/src/fonts.zip
 sudo mv $PWD/*.ttf /usr/share/fonts/truetype/hack
 mkdir ~/.config/i3
 mkdir ~/.config/terminator
-mkdir ~/.config/bumblebee-status
+#mkdir ~/.config/bumblebee-status
 mkdir ~/.config/compton
 mkdir ~/.config/rofi
 mkdir ~/.config/wallpaper
 cp $PWD/src/i3/* ~/.config/i3
 cp $PWD/src/terminator/* ~/.config/terminator
-cp $PWD/src/bumblebee-status/* ~/.config/bumblebee-status
+#cp $PWD/src/bumblebee-status/* ~/.config/bumblebee-status
 cp $PWD/src/compton/* ~/.config/compton
 cp $PWD/src/rofi/* ~/.config/rofi
 cp $PWD/src/wallpaper/* ~/.config/wallpaper
