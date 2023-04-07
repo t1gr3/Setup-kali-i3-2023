67638cd0903a8a030a2baf4de736aac00fefe82a
t1gr3 <wamg-85@hotmail.com>
2023-04-07 00:12:47 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,20 +6,17 @@ unzip $PWD/src/fonts.zip
 sudo mv $PWD/*.ttf /usr/share/fonts/truetype/hack
 mkdir ~/.config/i3
 mkdir ~/.config/terminator
-#mkdir ~/.config/bumblebee-status
 mkdir ~/.config/compton
 mkdir ~/.config/rofi
 mkdir ~/.config/wallpaper
 cp $PWD/src/i3/* ~/.config/i3
 cp $PWD/src/terminator/* ~/.config/terminator
-#cp -p $PWD/bumblebee-status/* $HOME/.config/bumblebee-status
 cp $PWD/src/compton/* ~/.config/compton
 cp $PWD/src/rofi/* ~/.config/rofi
 cp $PWD/src/wallpaper/* ~/.config/wallpaper
 cd "$HOME/.config/"
 git clone https://github.com/tobi-wan-kenobi/bumblebee-status.git
-sleep 50
-#sudo chmod +x ~/.config/bumblebee-status/*
+sleep 20
 
 sudo apt autoremove -y
 sudo reboot
