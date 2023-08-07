d247bdd810e2f4a215dd6a2d3a6d1033f4724a1a
t1gr3 <wamg-85@hotmail.com>
2023-08-07 16:23:34 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install i3 dolphin terminator rofi feh flameshot compton python3-psutil -y
-unzip $PWD/src/fonts.zip
-sudo mv $PWD/*.ttf /usr/share/fonts/truetype/hack
+sudo apt install i3 dolphin rofi feh flameshot python3-psutil -y
+#unzip $PWD/src/fonts.zip
+#sudo mv $PWD/*.ttf /usr/share/fonts/truetype/hack
 mkdir ~/.config/i3
-mkdir ~/.config/terminator
-mkdir ~/.config/compton
 mkdir ~/.config/rofi
 mkdir ~/.config/wallpaper
 cp $PWD/src/i3/* ~/.config/i3
-cp $PWD/src/terminator/* ~/.config/terminator
-cp $PWD/src/compton/* ~/.config/compton
 cp $PWD/src/rofi/* ~/.config/rofi
 cp $PWD/src/wallpaper/* ~/.config/wallpaper
 cd "$HOME/.config/"
